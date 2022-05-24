ef83cd7631d64b336142e1c4e5d209a8728a5045
Matthew Carson <matthew.carson@pm.me>
2022-05-24 11:27:45 -0400

    Move secret to .env

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const jwt = require('jsonwebtoken');
 
-const secret = 'mysecretssshhhhhhh';
+const secret = process.env.APP_KEY;
 const expiration = '2h';
 
 module.exports = {
