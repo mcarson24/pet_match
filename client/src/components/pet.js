a915e844f4e49df249003a8601095e0172d75f25
Matthew Carson <matthew.carson@pm.me>
2022-05-25 12:55:02 -0400

    Get pets

--- client/src/components/pet.js
+++ client/src/components/pet.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 import '../styles/pet.css';
@@ -7,7 +7,8 @@ import '../styles/pet.css';
 function Pet() {
     const [pets, setPets] = useState([])
 
-    const getPets = () => {
+    const getPets = e => {
+        e.preventDefault()
         axios.get('http://localhost:3001/api/token')
              .then(res => {
                  setPets(res.data.animals)
@@ -27,25 +28,24 @@ function Pet() {
                 <div className='row justify-content-center'>
                     <div className='col-10 searchCol'>
 
-                        <form className="form-inline searchBar">
+                        <form className="form-inline searchBar" onSubmit={e => getPets(e)}>
                             <input className="form-control mr-sm-2" type="search" placeholder="Search Pets" aria-label="Search" />
-                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit"
-                                    onClick={() => getPets()}
-                                    >
-                                    Search
-                            </button>
+                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                         </form>
 
-                    </div>
-                </div>
-                <div>
-                    {pets.map(pet => (
-                        <div key={pet.id}>
-                            <h2>{ pet.name }</h2>
-                            <h4>{ pet.age } | { pet.gender } | { pet.size }</h4>
-                            <img src={pet.photos[0].large} />
+                        <div>
+                            {pets.map(pet => (
+                                <div key={pet.id}>
+                                    <div>
+                                        <h2>{ pet.name }</h2>
+                                    </div>
+
+                                    <h4>{ pet.age } | { pet.gender } | { pet.size }</h4>
+                                    <img src={pet.photos[0].large} />
+                                </div>
+                            ))}
                         </div>
-                    ))}
+                    </div>
                 </div>
             </div>
         </>
