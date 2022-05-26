d3f70ec7ad4fab139fe33be05fe27a8321eeaf69
Matthew Carson <matthew.carson@pm.me>
2022-05-26 19:46:25 -0400

    Heroku updates

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,16 +2,9 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 
 // Configure Mongoose and MongoDB
-if (process.env.MONGODB_URI) {
-  mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })  
-} else {
-  mongoose.connect('mongodb://localhost:27017/pet_match', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-}
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pet_match', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})  
 
 module.exports = mongoose.connection
