4340b16de57fbdd7c864b9b82ff607b2f9735b3a
Matthew Carson <matthew.carson@pm.me>
2022-05-26 19:38:03 -0400

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,17 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 
 // Configure Mongoose and MongoDB
-mongoose.connect('mongodb://localhost:27017/pet_match', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })  
+} else {
+  mongoose.connect('mongodb://localhost:27017/pet_match', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+}
 
 module.exports = mongoose.connection
