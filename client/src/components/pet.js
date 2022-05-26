eedcaa277be4a882968085d3628df9dc1a5359b6
Matthew Carson <matthew.carson@pm.me>
2022-05-26 10:01:34 -0400

    Style pets component

--- client/src/components/pet.js
+++ client/src/components/pet.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import Button from 'react-bootstrap-button-loader'
 
 import '../styles/pet.css';
 
@@ -7,12 +8,48 @@ import '../styles/pet.css';
 function Pet() {
     const [pets, setPets] = useState([])
 
+    const classes = {
+        svg: {
+            height: '1em'
+        },
+        petTitle: {
+            display: 'flex',
+            justifyContent: 'space-between',
+        },
+        petsList: {
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+        },
+        pet: {
+            margin: '1em auto',
+            backgroundColor: 'rgb(228 228 231)',
+            padding: '1em 2em',
+            width: '75%',
+            borderRadius: '1em',
+            filter: 'drop-shadow(0 4px 3px rgb(0 0 0 / 0.07)) drop-shadow(0 2px 2px rgb(0 0 0 / 0.06))'
+        },
+        petInfo: {
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            color: 'rgb(113 113 122)'
+        },
+        petImage: {
+            borderRadius: '1em'
+        }
+    }
+
     const getPets = e => {
         e.preventDefault()
         axios.get('http://localhost:3001/api/token')
              .then(res => {
                  setPets(res.data.animals)
              })
+    }
+
+    const likePet = pet => {
+        // Use mutator
     }
 
     return (
@@ -33,15 +70,22 @@ function Pet() {
                             <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                         </form>
 
-                        <div>
+                        <div style={classes.petsList}>
                             {pets.map(pet => (
-                                <div key={pet.id}>
-                                    <div>
+                                <div key={pet.id} style={classes.pet}>
+                                    <div style={ classes.petTitle }>
                                         <h2>{ pet.name }</h2>
+                                        <Button onClick={() => likePet(pet.id)}>
+                                            <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" style={classes.svg}>
+                                                <path style={ { fill: 'white' } } d="m10 3.22-.61-.6a5.5 5.5 0 0 0 -7.78 7.77l8.39 8.39 8.39-8.4a5.5 5.5 0 0 0 -7.78-7.77z"/>
+                                            </svg>
+                                        </Button>
                                     </div>
-
-                                    <h4>{ pet.age } | { pet.gender } | { pet.size }</h4>
-                                    <img src={pet.photos[0].large} />
+ 
+                                    <div style={classes.petInfo}>
+                                        <h5>{ pet.age } | { pet.gender } | { pet.size }</h5>
+                                        <img src={pet.photos[0].large} alt={pet.name} style={classes.petImage}/>
+                                    </div>
                                 </div>
                             ))}
                         </div>
