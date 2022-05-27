f4d55c94fc627683b4e21f8dfb317195ad61a723
Matthew Carson <matthew.carson@pm.me>
2022-05-26 20:01:30 -0400

    I hate heroku

--- server/controllers/api_controller.js
+++ server/controllers/api_controller.js
@@ -18,6 +18,7 @@ module.exports = {
       
       res.status(200)
          .setHeader("Access-Control-Allow-Origin", process.env.APP_URL)
+         .setHeader("Access-Control-Allow-Credentials", true)
          .json(response.data)
     } catch (err) {
       console.log(err)
