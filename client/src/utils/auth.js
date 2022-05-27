6b1816a164b785baf376b3733268f43a9c991a81
Matthew Carson <matthew.carson@pm.me>
2022-05-26 20:12:55 -0400

    Update auth.js

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -33,7 +33,7 @@ class AuthService {
 
   login(idToken) {
     localStorage.setItem('id_token', idToken);
-    window.location.assign('/profile');
+    window.location.assign('/#/profile');
   }
 
   logout() {
