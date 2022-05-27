7d6e2d4bce95ba2a77e768b02f57b622b435890e
Matthew Carson <matthew.carson@pm.me>
2022-05-26 20:10:18 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import Nav from './components/nav'
 import './App.css';
 import {
